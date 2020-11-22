4443a02d0c45f9dc729975b94407ee0fa056ef5d
Liliia Yarmolova <lyarmolova@ukr.net>
2020-11-22 20:29:56 +0200

    Добавляю пролистывание изображений галереи в открытом модальном окне

--- src/gallery-create.js
+++ src/gallery-create.js
@@ -1,13 +1,20 @@
-import items from "./gallery-items.js"
+import galleryItems from "./gallery-items.js"
+
+const galleryRef = document.querySelector('.js-gallery')
+const largeImage = document.querySelector('.lightbox__image')
+const wrapper = document.querySelector('.lightbox') 
+const overlayRef = document.querySelector('.lightbox__overlay')
+const btnClose = document.querySelector('button[data-action="close-lightbox"]')
 
 // Создание и рендер разметки по массиву данных и предоставленному шаблону.
-const createList = (array) => array.map(({preview, original, description}) => {
+const createList = (array) => array.map(({preview, original, description}, index) => {
     const listItem = document.createElement('li')
     const listItemLink = document.createElement('a')
     const listItemImg = document.createElement('img')
     listItemLink.setAttribute('href', original)
     listItemImg.setAttribute('src', preview)
     listItemImg.setAttribute('data-source', original)
+    listItemImg.setAttribute('data-index', index)
     listItemImg.setAttribute('alt', description)
     listItem.classList.add('gallery__item')
     listItemLink.classList.add('gallery__link')
@@ -17,21 +24,93 @@ const createList = (array) => array.map(({preview, original, description}) => {
     return listItem
 });
 
-const galleryRef = document.querySelector('.js-gallery')
-const galleryItems = createList(items)
-galleryRef.append(...galleryItems)
-// console.log(galleryItems)
-// console.log(galleryRef)
+const gallery = createList(galleryItems)
+galleryRef.append(...gallery)
+
  
 // Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
-galleryRef.addEventListener('click', getLargePicture)
+galleryRef.addEventListener('click', onClick)
 
-function getLargePicture(event) {
-    event.preventDefaulf()
-    if (event.target.nodeName !== 'IMG') {
+
+function onClick (event) {
+    event.preventDefault()
+    const imageRef = event.target
+
+    if (imageRef.nodeName !== 'IMG') {
         return
     }
-    // const lagrePicture = event.target.dataset.source
-    console.log(event.target)
-    console.log (event.target.dataset.source)
-}
+    
+    
+    const lagreImageURL = event.target.dataset.source
+    const lagreImageAlt = event.target.alt
+    const lagreImageIndex = event.target.dataset.index
+    onModalOpen()
+
+    // Подмена значения атрибута src элемента img.lightbox__image.
+    largeImage.src = lagreImageURL
+    largeImage.alt = lagreImageAlt
+    largeImage.setAttribute('data-id', lagreImageIndex)
+ }
+
+
+
+   
+// Открытие модального окна по клику на элементе галереи.
+function onModalOpen() {
+    // Закрытие модального окна по нажатию клавиши ESC.
+    window.addEventListener('keydown', onPressEsc)
+    window.addEventListener('keydown', onLeftRightPress)
+  
+    wrapper.classList.add('is-open')
+    
+    }
+
+    // Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
+    btnClose.addEventListener('click', onModalClose)
+
+    function onModalClose() {
+        window.removeEventListener('keydown', onPressEsc)
+        wrapper.classList.remove('is-open')
+
+        // Очистка значения атрибута src элемента img.lightbox__image.
+        clearSource()
+    }
+
+    // Очистка значения атрибута src элемента img.lightbox__image.
+    // Это необходимо для того, чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
+
+    function clearSource() {
+        largeImage.src = ''
+        // imageRef.classList.('current')
+    }
+
+    // Закрытие модального окна по клику на div.lightbox__overlay.
+    overlayRef.addEventListener('click', onOverlayClick)
+    function onOverlayClick(event) {
+        if (event.target === event.currentTarget) {
+            onModalClose()
+        }
+    }
+
+    // Закрытие модального окна по нажатию клавиши ESC.
+    function onPressEsc(event) {
+        if (event.code === 'Escape') {
+            onModalClose()
+        }
+    }
+
+    // Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
+function onLeftRightPress(event) {
+    let currentIndex = Number(event.target.firstElementChild.dataset.index)
+     if (event.code === 'ArrowRight') {
+               
+         largeImage.src = (currentIndex === galleryItems.length) ? galleryItems[0].original : galleryItems[currentIndex+1].original
+      
+    } else if (event.code === 'ArrowLeft') {
+          largeImage.src = (currentIndex === 0) ? galleryItems[galleryItems.length-1].original : galleryItems[currentIndex-1].original
+    
+    }
+    }
+
+
+   
